1fcf356f2cd820f03f383aa84bd973ad33bf0c70
Notepad96 <notepad1029@gmail.com>
2024-06-22 02:37:48 +0900

    Complete Project

--- Scaffold/app/src/main/java/sps/notepad96/scaffold/MainActivity.kt
+++ Scaffold/app/src/main/java/sps/notepad96/scaffold/MainActivity.kt
@@ -3,13 +3,25 @@ package sps.notepad96.scaffold
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.BottomAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FabPosition
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.graphics.Color
 import sps.notepad96.scaffold.ui.theme.ScaffoldTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +34,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    Main()
                 }
             }
         }
@@ -30,17 +42,48 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
+fun Main() {
+    Scaffold(
+        topBar = { MyTopBar() },
+        content = {
+            MyContents(it)
+        },
+        bottomBar = { MyBottomBar() },
+
+        floatingActionButton = {
+            FloatingActionButton(onClick = { /*TODO*/ }) {
+                Icon(imageVector = Icons.Default.Add, contentDescription = "add")
+            }
+        },
+        floatingActionButtonPosition = FabPosition.End
     )
 }
 
-@Preview(showBackground = true)
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun GreetingPreview() {
-    ScaffoldTheme {
-        Greeting("Android")
+fun MyTopBar() {
+    TopAppBar(colors = TopAppBarDefaults.topAppBarColors(containerColor = Color.Red),
+        title = {
+            Text(text = "Top Bar")
+        }
+    )
+}
+
+@Composable
+fun MyContents(innerPadding: PaddingValues) {
+    Surface(
+        modifier = Modifier
+            .padding(innerPadding)
+            .fillMaxSize(),
+        color = Color.LightGray
+    ) {
+        Text(text = "Content")
+    }
+}
+
+@Composable
+fun MyBottomBar() {
+    BottomAppBar(containerColor = Color.Yellow) {
+        Text(text = "Bottom Bar")
     }
 }
