f88a95514d165b6553704b42ea7c44f9c0151a71
Notepad96 <notepad1029@gmail.com>
2024-06-25 21:01:44 +0900

    Create Project

---
+++ ListGrid/app/src/main/java/sps/notepad96/listgrid/MainActivity.kt
@@ -0,0 +1,100 @@
+package sps.notepad96.listgrid
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyHorizontalGrid
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import sps.notepad96.listgrid.ui.theme.ListGridTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            ListGridTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Main(innerPadding)
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Main(innerPadding: PaddingValues) {
+    var selected by remember { mutableIntStateOf(0) }
+
+    Column(
+        modifier = Modifier
+            .padding(innerPadding)
+            .fillMaxSize()
+    ) {
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceEvenly
+        ) {
+            Button(onClick = { selected = 0 }) {
+                Text(text = "Column Grid List")
+            }
+            Button(onClick = { selected = 1 }) {
+                Text(text = "Row Grid List")
+            }
+        }
+
+        when(selected) {
+            0 -> {
+                LazyVerticalGrid(columns = GridCells.Fixed(3)) {
+                    items(50) {
+                        ListGridItem(it)
+                    }
+                }
+            }
+            1 -> {
+                LazyHorizontalGrid(rows = GridCells.Adaptive(minSize = 350.dp)) {
+                    items(50) {
+                        ListGridItem(it)
+                    }
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun ListGridItem(index: Int) {
+    val colors = listOf(Color.Cyan, Color.Yellow, Color.Green)
+    Column(
+        modifier = Modifier.width(100.dp).height(100.dp).background(colors.random()),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Text(text = "item $index")
+    }
+}
