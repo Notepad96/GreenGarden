cd59bcf44661b8eb27ea3872e010d939927b8dac
Notepad96 <notepad1029@gmail.com>
2024-07-10 01:36:54 +0900

    Update Preview

--- ListGrid/README.md
+++ ListGrid/README.md
@@ -1,5 +1,5 @@
 # Preview
-https://greengarden9.blogspot.com/2024/06/compose-list-grid-lazyverticalgrid.html
+https://www.composeworld.xyz/2024/07/compose-grid-list-lazyverticalgrid.html
 <br/><br/>
 
 ![preview](preview.gif)
--- ListGrid/app/src/main/java/sps/notepad96/listgrid/MainActivity.kt
+++ ListGrid/app/src/main/java/sps/notepad96/listgrid/MainActivity.kt
@@ -17,8 +17,8 @@ import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyHorizontalGrid
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.itemsIndexed
 import androidx.compose.material3.Button
-import androidx.compose.material3.Card
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -29,7 +29,6 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import sps.notepad96.listgrid.ui.theme.ListGridTheme
 
@@ -50,6 +49,8 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun Main(innerPadding: PaddingValues) {
     var selected by remember { mutableIntStateOf(0) }
+    val sample = mutableListOf<String>()
+    for (i in 1..50) sample.add("$i:Text")
 
     Column(
         modifier = Modifier
@@ -70,16 +71,16 @@ fun Main(innerPadding: PaddingValues) {
 
         when(selected) {
             0 -> {
-                LazyVerticalGrid(columns = GridCells.Fixed(3)) {
-                    items(50) {
-                        ListGridItem(it)
+                LazyVerticalGrid(columns = GridCells.Adaptive(minSize = 200.dp)) {
+                    itemsIndexed(sample) { index, text ->
+                        ListGridItem(index, text)
                     }
                 }
             }
             1 -> {
-                LazyHorizontalGrid(rows = GridCells.Adaptive(minSize = 350.dp)) {
+                LazyHorizontalGrid(rows = GridCells.Fixed(3)) {
                     items(50) { index ->
-                        ListGridItem(index)
+                        ListGridItem(index, "item")
                     }
                 }
             }
@@ -88,13 +89,17 @@ fun Main(innerPadding: PaddingValues) {
 }
 
 @Composable
-fun ListGridItem(index: Int) {
-    val colors = listOf(Color.Cyan, Color.Yellow, Color.Green)
+fun ListGridItem(index: Int, text: String) {
+    val widths = listOf(150.dp, 200.dp, 250.dp)
+    val colors = listOf(Color(222, 193, 240, 255),
+        Color(221, 243, 181, 255),
+        Color(172, 215, 243, 255)
+    )
     Column(
-        modifier = Modifier.width(100.dp).height(100.dp).background(colors.random()),
+        modifier = Modifier.width(widths.random()).height(100.dp).background(colors.random()),
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center
     ) {
-        Text(text = "item $index")
+        Text(text = "$text $index")
     }
 }
