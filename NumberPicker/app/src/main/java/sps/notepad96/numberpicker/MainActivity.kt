eb30dd3a9583f528d89c813e76b0ee520b026f8b
Notepad96 <notepad1029@gmail.com>
2024-07-22 01:55:05 +0900

    Complete Project

--- NumberPicker/app/src/main/java/sps/notepad96/numberpicker/MainActivity.kt
+++ NumberPicker/app/src/main/java/sps/notepad96/numberpicker/MainActivity.kt
@@ -1,16 +1,41 @@
 package sps.notepad96.numberpicker
 
+import android.content.Context
 import android.os.Bundle
+import android.widget.NumberPicker
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.Card
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.compose.ui.viewinterop.AndroidView
+import androidx.compose.ui.window.Dialog
 import sps.notepad96.numberpicker.ui.theme.NumberPickerTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,10 +45,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             NumberPickerTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    Main(innerPadding)
                 }
             }
         }
@@ -31,17 +53,84 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun Main(innerPadding: PaddingValues) {
+    val context = LocalContext.current
+    var showDialog by remember { mutableStateOf(false) }
+    var number by remember { mutableIntStateOf(0) }
+
+    if (showDialog) {
+        MyNumberPicker(
+            context = context,
+            onDismissRequest = { showDialog = false },
+            onConfirmation = {
+                number = it
+                showDialog = false
+            },
+            default = number
+        )
+    }
+
+    Column(
+        modifier = Modifier
+            .padding(innerPadding)
+            .fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.SpaceEvenly
+    ) {
+        Button(onClick = { showDialog = true }) {
+            Text(text = "Open")
+        }
+        Text(text = "$number", fontSize = 32.sp)
+    }
+
 }
 
-@Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    NumberPickerTheme {
-        Greeting("Android")
+fun MyNumberPicker(
+    context: Context,
+    onDismissRequest: () -> Unit,
+    onConfirmation: (Int) -> Unit,
+    default: Int
+) {
+    val numberPicker = NumberPicker(context)
+    numberPicker.minValue = 0
+    numberPicker.maxValue = 100
+    numberPicker.value = default
+
+    Dialog(onDismissRequest = { onDismissRequest() }) {
+        Card(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(300.dp)
+                .padding(16.dp),
+            shape = RoundedCornerShape(16.dp),
+        ) {
+            Column(
+                modifier = Modifier
+                    .fillMaxSize(),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally,
+            ) {
+                AndroidView(factory = { numberPicker })
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth(),
+                    horizontalArrangement = Arrangement.Center,
+                ) {
+                    TextButton(
+                        onClick = { onDismissRequest() },
+                        modifier = Modifier.padding(8.dp),
+                    ) {
+                        Text("취소")
+                    }
+                    TextButton(
+                        onClick = { onConfirmation(numberPicker.value) },
+                        modifier = Modifier.padding(8.dp),
+                    ) {
+                        Text("확인")
+                    }
+                }
+            }
+        }
     }
 }
