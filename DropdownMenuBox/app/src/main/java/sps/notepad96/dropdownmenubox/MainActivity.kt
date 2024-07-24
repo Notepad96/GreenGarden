139ad6714bac53eaceb48ffff55fc21078caba70
Notepad96 <notepad1029@gmail.com>
2024-07-24 23:28:24 +0900

    Complete Proejct

--- DropdownMenuBox/app/src/main/java/sps/notepad96/dropdownmenubox/MainActivity.kt
+++ DropdownMenuBox/app/src/main/java/sps/notepad96/dropdownmenubox/MainActivity.kt
@@ -4,13 +4,30 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ExposedDropdownMenuBox
+import androidx.compose.material3.ExposedDropdownMenuDefaults
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
 import sps.notepad96.dropdownmenubox.ui.theme.DropdownMenuBoxTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,28 +37,48 @@ class MainActivity : ComponentActivity() {
         setContent {
             DropdownMenuBoxTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    Main(innerPadding)
                 }
             }
         }
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+fun Main(innerPadding: PaddingValues) {
+    val sample = listOf("item01", "item02", "item03", "item04")
+    var text by remember { mutableStateOf("") }
+    var expanded by remember { mutableStateOf(false) }
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    DropdownMenuBoxTheme {
-        Greeting("Android")
+    Column(
+        modifier = Modifier
+            .padding(innerPadding)
+            .fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.SpaceAround
+    ) {
+        Text(text = text, fontSize = 24.sp)
+
+        ExposedDropdownMenuBox(expanded = expanded, onExpandedChange = { expanded = !expanded }) {
+            OutlinedTextField(
+                value = text, onValueChange = {}, modifier = Modifier.menuAnchor(),
+                label = { Text("Options") },
+                readOnly = true,
+                trailingIcon = {
+                    ExposedDropdownMenuDefaults.TrailingIcon(expanded = expanded)
+                }
+            )
+
+            ExposedDropdownMenu(expanded = expanded, onDismissRequest = { expanded = false },
+                modifier = Modifier.background(Color(255, 209, 209, 255))) {
+                sample.forEach { item ->
+                    DropdownMenuItem(text = { Text(item) }, onClick = {
+                        text = item
+                        expanded = false
+                    })
+                }
+            }
+        }
     }
 }
