1d395e72f5b6ce7cdfed31ad8ec23081ef138b4c
Notepad96 <notepad1029@gmail.com>
2024-07-27 22:16:27 +0900

    Create Proejct

---
+++ ToastMsg/app/src/main/java/sps/notepad96/toastmsg/MainActivity.kt
@@ -0,0 +1,34 @@
+package sps.notepad96.toastmsg
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import sps.notepad96.toastmsg.ui.theme.ToastMsgTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            ToastMsgTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Main(innerPadding)
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Main(innerPadding: PaddingValues) {
+
+}
