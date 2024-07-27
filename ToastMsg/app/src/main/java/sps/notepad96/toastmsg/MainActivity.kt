dee116325d00b703d589e11b902f7567dab83aeb
Notepad96 <notepad1029@gmail.com>
2024-07-27 23:41:02 +0900

    Complete Project

--- ToastMsg/app/src/main/java/sps/notepad96/toastmsg/MainActivity.kt
+++ ToastMsg/app/src/main/java/sps/notepad96/toastmsg/MainActivity.kt
@@ -4,14 +4,33 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Info
+import androidx.compose.material3.Button
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Snackbar
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.unit.dp
+import kotlinx.coroutines.launch
 import sps.notepad96.toastmsg.ui.theme.ToastMsgTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,15 +39,62 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             ToastMsgTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Main(innerPadding)
-                }
+                Main()
             }
         }
     }
 }
 
 @Composable
-fun Main(innerPadding: PaddingValues) {
+fun Main() {
+    val snackBarHostState = remember { SnackbarHostState() }
+    val coroutineScope = rememberCoroutineScope()
 
+    Scaffold(modifier = Modifier.fillMaxSize(),
+        snackbarHost = {
+            SnackbarHost(hostState = snackBarHostState,
+                modifier = Modifier.offset(y = (-100).dp)
+            ) {
+                CustomSnackBar(icon = Icons.Filled.Info, message = it.visuals.message)
+            }
+        }) { innerPadding ->
+        Column(
+            modifier = Modifier
+                .padding(innerPadding)
+                .fillMaxSize(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Button(onClick = {
+                coroutineScope.launch {
+                    snackBarHostState.showSnackbar("Show Message")
+                }
+            }) {
+                Text("Toast Msg")
+            }
+        }
+    }
+}
+
+@Composable
+fun CustomSnackBar(
+    icon: ImageVector,
+    message: String,
+    containerColor: Color = Color(60, 60, 60, 255)
+) {
+    Snackbar(
+        modifier = Modifier.width(180.dp),
+        shape = RoundedCornerShape(16.dp),
+        containerColor = containerColor
+    ) {
+        Row(
+            modifier = Modifier.fillMaxSize(),
+            horizontalArrangement = Arrangement.Center,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Icon(icon, contentDescription = null, tint = Color(94, 255, 115, 255))
+            Spacer(modifier = Modifier.width(8.dp))
+            Text(message)
+        }
+    }
 }
