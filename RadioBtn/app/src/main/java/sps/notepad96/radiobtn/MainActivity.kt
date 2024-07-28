44ab087798459dced2aefcca82eea5e28082d85e
Notepad96 <notepad1029@gmail.com>
2024-07-28 23:10:51 +0900

    Complete Project

--- RadioBtn/app/src/main/java/sps/notepad96/radiobtn/MainActivity.kt
+++ RadioBtn/app/src/main/java/sps/notepad96/radiobtn/MainActivity.kt
@@ -4,13 +4,31 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.text.ClickableText
+import androidx.compose.material3.RadioButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.draw.scale
+import androidx.compose.ui.text.AnnotatedString
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import sps.notepad96.radiobtn.ui.theme.RadioBtnTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,10 +38,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             RadioBtnTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    Main(innerPadding)
                 }
             }
         }
@@ -31,17 +46,34 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+fun Main(innerPadding: PaddingValues) {
+    val items = listOf("Option 1", "Option 2", "Option 3")
+    var selectedItem by remember { mutableStateOf("Option") }
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    RadioBtnTheme {
-        Greeting("Android")
+    Column(
+        modifier = Modifier
+            .padding(innerPadding)
+            .fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Text(text = selectedItem, fontSize = 28.sp)
+        Spacer(modifier = Modifier.height(18.dp))
+        items.forEach { item ->
+            Row(verticalAlignment = Alignment.CenterVertically) {
+                RadioButton(
+                    selected = (item == selectedItem),
+                    onClick = { selectedItem = item },
+                    modifier = Modifier
+                        .scale(.8f)
+                        .size(40.dp)
+                        .padding(0.dp)
+                )
+                ClickableText(text = AnnotatedString(item),
+                    modifier = Modifier.padding(end = 4.dp),
+                    style = TextStyle(fontSize = 18.sp),
+                    onClick = { selectedItem = item })
+            }
+        }
     }
 }
