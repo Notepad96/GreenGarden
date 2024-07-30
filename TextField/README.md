a28868d5ceea1673d5ada032bfae46ff3572eac5
Notepad96 <notepad1029@gmail.com>
2024-07-30 23:46:16 +0900

    Update Preview

---
+++ TextField/README.md
@@ -0,0 +1,7 @@
+# Preview
+https://www.composeworld.xyz/2024/07/compose-textfield-outlinedtextfield.html
+<br/><br/>
+
+![preview](1.gif)
+<br/>
+![preview](2.gif)
--- TextField/app/src/main/java/sps/notepad96/textfield/MainActivity.kt
+++ TextField/app/src/main/java/sps/notepad96/textfield/MainActivity.kt
@@ -4,13 +4,37 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Info
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
+import androidx.core.text.isDigitsOnly
 import sps.notepad96.textfield.ui.theme.TextFieldTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,10 +44,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             TextFieldTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    Main(innerPadding)
                 }
             }
         }
@@ -31,17 +52,65 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
+fun Main(innerPadding: PaddingValues) {
+    var text01 by remember { mutableStateOf("") }
+    var text02 by remember { mutableStateOf("123xxx") }
+
+    Column(
+        modifier = Modifier
+            .padding(innerPadding)
+            .fillMaxSize(),
+        verticalArrangement = Arrangement.SpaceAround,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Column {
+            Text(text = "text01 : $text01")
+            MyTextField(text = text01, setOnTextChange = { text01 = it })
+        }
+        Column {
+            Text(text = "text02 : $text02")
+            MyOutlinedTextField(text = text02, setOnTextChange = { text02 = it })
+        }
+    }
+}
+
+@Composable
+fun MyTextField(
+    text: String,
+    setOnTextChange: (String) -> Unit
+) {
+    TextField(
+        value = text, onValueChange = { setOnTextChange(it) },
+        colors = TextFieldDefaults.colors(
+//            focusedContainerColor = Color.Transparent,
+            unfocusedContainerColor = Color.Transparent,
+            focusedIndicatorColor = Color.Green,
+            unfocusedIndicatorColor = Color.Blue
+        ),
+        textStyle = TextStyle(textAlign = TextAlign.End, fontSize = 20.sp)
     )
 }
 
-@Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    TextFieldTheme {
-        Greeting("Android")
-    }
+fun MyOutlinedTextField(
+    text: String,
+    setOnTextChange: (String) -> Unit
+) {
+    OutlinedTextField(
+        value = text, onValueChange = { setOnTextChange(it) },
+        label = { Text(text = "Name") },
+        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+        isError = !text.isDigitsOnly(),
+        trailingIcon = {
+            if (!text.isDigitsOnly()) {
+                Icon(imageVector = Icons.Filled.Info, contentDescription = "error",
+                    tint = MaterialTheme.colorScheme.error)
+            }
+        },
+        supportingText = {
+            if (!text.isDigitsOnly()) {
+                Text(text = "Only Number!!", fontSize = 14.sp)
+            }
+        }
+    )
 }
