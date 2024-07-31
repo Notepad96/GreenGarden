15b04e8355f390d32adae0e554ea29c9fffac745
Notepad96 <notepad1029@gmail.com>
2024-08-01 00:22:02 +0900

    Complete Project

--- DatePickerDialog/app/src/main/java/sps/notepad96/datepickerdialog/MainActivity.kt
+++ DatePickerDialog/app/src/main/java/sps/notepad96/datepickerdialog/MainActivity.kt
@@ -4,15 +4,29 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.DatePicker
+import androidx.compose.material3.DatePickerDialog
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SelectableDates
 import androidx.compose.material3.Text
+import androidx.compose.material3.rememberDatePickerState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import sps.notepad96.datepickerdialog.ui.theme.DatePickerDialogTheme
+import java.text.SimpleDateFormat
+import java.util.Date
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -30,5 +44,66 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Main(innerPadding: PaddingValues) {
+    var date by remember { mutableStateOf("Open date picker dialog") }
+    var showDatePicker by remember { mutableStateOf(false) }
+    if (showDatePicker) {
+        MyDatePickerDialog(
+            onDateSelected = { date = it },
+            onDismiss = { showDatePicker = false }
+        )
+    }
 
+    Box(modifier = Modifier.padding(innerPadding).fillMaxSize(),
+        contentAlignment = Alignment.Center) {
+        Button(onClick = { showDatePicker = true }) {
+            Text(text = date)
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun MyDatePickerDialog(
+    onDateSelected: (String) -> Unit,
+    onDismiss: () -> Unit
+) {
+    val datePickerState = rememberDatePickerState(selectableDates = object : SelectableDates {
+        override fun isSelectableDate(utcTimeMillis: Long): Boolean {
+            return utcTimeMillis <= System.currentTimeMillis()
+        }
+    })
+
+    val selectedDate = datePickerState.selectedDateMillis?.let {
+        convertMillisToDate(it)
+    } ?: ""
+
+    DatePickerDialog(
+        onDismissRequest = { onDismiss() },
+        confirmButton = {
+            Button(onClick = {
+                onDateSelected(selectedDate)
+                onDismiss()
+            }
+
+            ) {
+                Text(text = "OK")
+            }
+        },
+        dismissButton = {
+            Button(onClick = {
+                onDismiss()
+            }) {
+                Text(text = "Cancel")
+            }
+        }
+    ) {
+        DatePicker(
+            state = datePickerState
+        )
+    }
+}
+
+private fun convertMillisToDate(millis: Long): String {
+    val formatter = SimpleDateFormat("yyyy-MM-dd")
+    return formatter.format(Date(millis))
 }
