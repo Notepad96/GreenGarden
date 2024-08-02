49b92088c7f13a6b2c4724af41feffb978396918
Notepad96 <notepad1029@gmail.com>
2024-08-02 23:35:27 +0900

    Test: Toast msg

---
+++ PlayGround/app/src/main/java/sps/notepad96/playground/MainActivity.kt
@@ -0,0 +1,52 @@
+package sps.notepad96.playground
+
+import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import sps.notepad96.playground.ui.theme.PlayGroundTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            PlayGroundTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Main(innerPadding)
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Main(innerPadding: PaddingValues) {
+    val context = LocalContext.current
+
+    Column(
+        modifier = Modifier
+            .padding(innerPadding)
+            .fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Button(onClick = { Toast.makeText(context, "Message", Toast.LENGTH_SHORT).show() }) {
+            Text("Alarm")
+        }
+    }
+
+}
