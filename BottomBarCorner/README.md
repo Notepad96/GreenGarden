bc3568498807ecf8f7508bbb82e80de4668b7aec
Notepad96 <notepad1029@gmail.com>
2024-08-08 08:10:44 +0900

    Update Preview

---
+++ BottomBarCorner/README.md
@@ -0,0 +1,5 @@
+# Preview
+https://www.composeworld.xyz/2024/08/compose-bottombar-transparent-background.html
+<br/><br/>
+
+![preview](preview.jpg)
--- BottomBarCorner/app/src/main/java/sps/notepad96/bottombarcorner/MainActivity.kt
+++ BottomBarCorner/app/src/main/java/sps/notepad96/bottombarcorner/MainActivity.kt
@@ -4,13 +4,28 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
+import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 import sps.notepad96.bottombarcorner.ui.theme.BottomBarCornerTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,29 +34,41 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             BottomBarCornerTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                Main()
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    BottomBarCornerTheme {
-        Greeting("Android")
+fun Main() {
+    Scaffold { innerPadding ->
+        Box(
+            modifier = Modifier
+                .padding(innerPadding)
+                .fillMaxSize()
+        ) {
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .background(Color.Yellow)
+            ) { }
+            Surface(
+                modifier = Modifier
+                    .height(100.dp)
+                    .clip(RoundedCornerShape(topStart = 40.dp, topEnd = 40.dp))
+                    .align(Alignment.BottomCenter),
+                color = Color.Green
+            ) {
+                Row(
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.SpaceEvenly
+                ) {
+                    IconButton(onClick = {}) { Icon(Icons.Filled.Add, contentDescription = "add") }
+                    IconButton(onClick = {}) { Icon(Icons.Filled.Add, contentDescription = "add") }
+                    IconButton(onClick = {}) { Icon(Icons.Filled.Add, contentDescription = "add") }
+                }
+            }
+        }
     }
 }
